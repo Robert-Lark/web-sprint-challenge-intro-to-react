f8a1391e6b4236146619b6bd27a69550864a0c08
Rob Lark <quietcalmrecords@Robs-iMac.local>
2020-06-27 18:47:30 -0700

    completed mvp

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -6,19 +6,41 @@ const Character = (props) => {
 	const WrapperDiv = styled.div`
 		width: 100%;
 		height: 100%;
+		border: 5px solid darkBlue;
+		margin-bottom: 10px;
+	`;
+	const InnerDiv = styled.div`
+		margin: 0px 150px 0px 150px;
+		box-shadow: 10px 10px azure;
+		background: beige;
 	`;
 
 	return (
 		<WrapperDiv>
 			<div className="App">
 				<h1 className="Header">{props.bio.name}</h1>
-				<h2>{props.bio.birth_year}</h2>
-				<h2>{props.bio.eye_color}</h2>
-				<h2>{props.bio.gender}</h2>
-				<h2>{props.bio.hair_color}</h2>
-				<h2>{props.bio.height}</h2>
-				<h2>{props.bio.mass}</h2>
-				<h2>{props.bio.skin_color}</h2>
+				<InnerDiv>
+					<h2>{props.bio.birth_year}</h2>
+				</InnerDiv>
+				<InnerDiv>
+					<h2>{props.bio.eye_color}</h2>
+				</InnerDiv>
+				<InnerDiv>
+					<h2>{props.bio.gender}</h2>
+				</InnerDiv>
+
+				<InnerDiv>
+					<h2>{props.bio.hair_color}</h2>
+				</InnerDiv>
+				<InnerDiv>
+					<h2>{props.bio.height}</h2>
+				</InnerDiv>
+				<InnerDiv>
+					<h2>{props.bio.mass}</h2>
+				</InnerDiv>
+				<InnerDiv>
+					<h2>{props.bio.skin_color}</h2>
+				</InnerDiv>
 			</div>
 		</WrapperDiv>
 	);
